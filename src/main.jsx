a70a91cdf15c4fb5f11e0207d2b788511fab63c1
ura1722 <birkovych.yurii@student.uzhnu.edu.ua>
2024-11-07 19:33:39 +0200

    admin_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,7 @@
-
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux';
-import './index.css'
-import store from './stores/store';
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>  
-    <App />
-  </Provider>,
-)
+  <App />
+);
